27e4bc930ef40981bbc0b8ac1c6a49aa103baee1
Théo Herman <hermantheo@hotmail.com>
2022-04-10 14:58:01 +0200

    still initializing

--- film-browser/src/App.js
+++ film-browser/src/App.js
@@ -1,12 +1,12 @@
-import logo from './netflims.PNG';
+import logo from './netflims.png';
 import './App.css';
+import './'
 
 function Header() {
   return (
     <div>
       <header className="header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Netflims</h1>
       </header>
     </div>
   );
